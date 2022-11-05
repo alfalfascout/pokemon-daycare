c797895a84828ebe3b1c6ec097b25d3791bc982c
Noah Garland <noahgarland02@gmail.com>
2022-11-05 01:45:19 -0500

    Added conditions to make the factorial list smaller. Also made other changes

--- Automation/AutomateTest.py
+++ Automation/AutomateTest.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from itertools import product
 import re
@@ -12,14 +11,20 @@ combinations = list(product(l, repeat=18))
 clist = 0
 
 ErrorCount = 1
+cprint = 1
 
-driver = webdriver.Chrome(r'C:\Users\Noah\Desktop\pokemon-daycare\Automation\chromedriver.exe')
+#chromedriver.exe in \pokemon-daycare\Atomation\chromedriver.exe
+driverpath = ''
+site1 = 'http://alfalfascout.github.io/pokemon-daycare/'
+site2 = 'https://mkwrs.com/destiny_knot/'
+
+driver = webdriver.Chrome(driverpath)
 driver.maximize_window()
-driver.get(r'C:\Users\Noah\Desktop\pokemon-daycare\index.html')
+driver.get(site1)
 
-cdriver = webdriver.Chrome(r'C:\Users\Noah\Desktop\pokemon-daycare\Automation\chromedriver.exe')
+cdriver = webdriver.Chrome(driverpath)
 cdriver.maximize_window()
-cdriver.get('https://mkwrs.com/destiny_knot/')
+cdriver.get(site2)
 
 select = Select(driver.find_element("name", "held-item"))
 select.select_by_value('destiny-knot')
@@ -55,7 +60,31 @@ while True:
   p2 = combinations[clist][6:12]
   desired = combinations[clist][12:18]
 
-  #rgba(101, 149, 78, 1) selected
+  #Used to see if combination is impossible. If it is, then skip
+  tcount = 0
+  count = 0
+  while count < 6:
+    if p1[count] == True or p2[count] == True:
+      if desired[count] == True:
+        tcount+=1
+    count+=1
+
+  if tcount < desired.count(True) - 1:
+    clist+=1
+    continue
+
+  count = 0
+  for x in desired:
+    if x == True:
+      if p1[count] == True or p2[count] == True:
+        break
+    count+=1
+
+  if count == 6:
+    clist+=1
+    continue
+
+  #rgba(101, 149, 78, 1) is True color
 
   count = 0
   for x in p1:
@@ -115,20 +144,20 @@ while True:
     count+=1
 
   driver.find_element("name", "goal-calculate").click()
-  time.sleep(2)
+  time.sleep(1)
   percentage = driver.find_element(By.XPATH, "/html/body/div/div[4]/p[1]/b").text
   percentage = re.findall("\d+\.\d+|\d+", percentage)
   percentage_1 = float(percentage[0])
   print(percentage_1)
 
   cdriver.find_element(By.XPATH, '/html/body/form/input').click()
-  time.sleep(2)
+  time.sleep(1)
   percentage = cdriver.find_element(By.XPATH, "/html/body/div/b[1]").text
   percentage = re.findall("\d+\.\d+|\d+", percentage)
   percentage_2 = float(percentage[0])
   print(percentage_2)
 
-  if percentage_1 != percentage_2:
+  if percentage_1 != percentage_2 or percentage_1 == 0 or percentage_2 == 0:
     with open(r'C:\Users\Noah\Desktop\pokemon-daycare\Automation\log.txt', 'a') as f:
       f.write('ERROR ' + str(ErrorCount) + 
       ":\nParent 1 IV's: " + str(p1) + 
@@ -137,5 +166,6 @@ while True:
       "\nPercentage for Daycare: " + str(percentage_1) + " Percentage for other site: " + str(percentage_2) + "\n\n")
       ErrorCount+=1
 
-  print('Test ' + str(clist) + ' complete')
+  print('Test ' + str(cprint) + ' complete')
+  cprint+=1
   clist+=1
